60d373bce4767152670a5d09f77374d24822881c
tegelverenn <tegelveren@gmail.com>
2025-06-17 01:49:13 +0300

    HelloCydeo Updated

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello Word");
-        System.out.println("Hello Cydeo");
+        System.out.println("Hello CYDEO");
         System.out.println("Hello Programmers");
         
     }
